31f14131f926a4e2ef602c286a07cd42baf92743
Ferretex <79171540+Ferretex@users.noreply.github.com>
2021-04-10 15:10:57 -0400

    Moving Walls + Secret Area Warp

--- Assets/Scripts/WarpZoneScript.cs
+++ Assets/Scripts/WarpZoneScript.cs
@@ -5,10 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class WarpZoneScript : MonoBehaviour
 {
+    public int levelID;
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.transform.CompareTag("Player"))
         {
+            if(levelID != 0)
+            {
+                SceneManager.LoadScene(levelID);
+            }else
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
